d1c11916bcae02a3a9eba21badcdcf0e771c463f
joizhang <zxlwhom@outlook.com>
2018-03-17 11:32:49 +0800

    first commit

---
+++ container/nonrepeating/nonrepeating_test.go
@@ -0,0 +1,62 @@
+package main
+
+import "testing"
+
+// 代码覆盖率:
+// 1. go test -coverprofile=c.out
+// 2. go tool cover -html=c.out
+func TestSubstr(t *testing.T) {
+	tests := []struct {
+		s   string
+		ans int
+	}{
+		// Normal cases
+		{"abcabcbb", 3},
+		{"pwwkew", 3},
+
+		// Edge cases
+		{"", 0},
+		{"b", 1},
+		{"bbbbbbbbb", 1},
+		{"abcabcabcd", 4},
+
+		// Chinese support
+		{"这里是慕课网", 6},
+		{"一二三二一", 3},
+		{"黑化肥挥发发灰会花飞灰化肥挥发发黑会飞花", 8},
+	}
+
+	for _, tt := range tests {
+		actual := lengthOfNonRepeatingSubStr(tt.s)
+		if actual != tt.ans {
+			t.Errorf("got %d for input %s; "+
+				"expected %d",
+				actual, tt.s, tt.ans)
+		}
+	}
+}
+
+// 性能测试：
+// 1. go test -bench .
+
+// pprof:
+// 1. go test -bench . -cpuprofile cpu.out
+// 2. go tool pprof cpu.out
+// 3. web
+func BenchmarkSubstr(b *testing.B) {
+	s := "黑化肥挥发发灰会花飞灰化肥挥发发黑会飞花"
+	for i := 0; i < 13; i++ {
+		s = s + s
+	}
+	b.Logf("len(s) = %d", len(s))
+	ans := 8
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		actual := lengthOfNonRepeatingSubStr(s)
+		if actual != ans {
+			b.Errorf("got %d for input %s; expected %d",
+				actual, s, ans)
+		}
+	}
+}
---
+++ queue/queue_test.go
@@ -0,0 +1,25 @@
+package queue
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestExampleQueue_Pop(t *testing.T) {
+	q := Queue{1}
+	q.Push(2)
+	q.Push(3)
+	fmt.Println(q.Pop())
+	fmt.Println(q.Pop())
+	fmt.Println(q.IsEmpty())
+
+	fmt.Println(q.Pop())
+	fmt.Println(q.IsEmpty())
+
+	// Output:
+	// 1
+	// 2
+	// false
+	// 3
+	// true
+}
