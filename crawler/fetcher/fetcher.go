7e799a171d59364e2443e5750b5948099c1aea24
suiwenfeng <suiwenfeng@live.cn>
2018-11-23 09:46:42 +0800

    add useragent header to fix 403 forbidden

--- crawler/fetcher/fetcher.go
+++ crawler/fetcher/fetcher.go
@@ -3,21 +3,29 @@ package fetcher
 import (
 	"bufio"
 	"fmt"
-	"golang.org/x/net/html/charset"
-	"golang.org/x/text/encoding"
-	"golang.org/x/text/encoding/unicode"
-	"golang.org/x/text/transform"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"time"
+
+	"golang.org/x/net/html/charset"
+	"golang.org/x/text/encoding"
+	"golang.org/x/text/encoding/unicode"
+	"golang.org/x/text/transform"
 )
 
 var rateLimiter = time.Tick(100 * time.Millisecond)
 
+// Fetch request
 func Fetch(url string) ([]byte, error) {
 	<-rateLimiter
-	resp, err := http.Get(url)
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36")
+	resp, err := client.Do(req)
 	if err != nil {
 		return nil, err
 	}
